d108113ee12d8aa93f7f939d9474c28d64442f11
wissal <wissal@LAPTOP-3JJN376D>
2024-04-03 23:21:38 +0000

    Ajoutez des fichiers projet.

---
+++ CalculateDamage/Program.cs
@@ -0,0 +1,11 @@
+﻿using CalculateDamage;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        SwordDamage swordDamage = new SwordDamage();
+
+    }
+}
